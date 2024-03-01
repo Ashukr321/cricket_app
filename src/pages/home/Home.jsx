c115e934160ee4424e0086aefc6086a25fa78b46
Ashukr321 <coderashukr321@gmail.com>
2024-03-01 10:33:57 +0530

    fix bug

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../components/hero/Hero'
+import Fetures from '../../components/Fetures'
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Fetures/>
     </div>
   )
 }
